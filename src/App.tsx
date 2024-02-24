33ae21d3a45e456f6798906e945498a7bc9881a2
namith1003 <namithspider@gmail.com>
2024-02-25 03:17:50 +0530

    added file directories for organization and added type script to the project for development

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+// @ts-ignore
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -14,4 +15,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// @ts-ignore
 reportWebVitals();
